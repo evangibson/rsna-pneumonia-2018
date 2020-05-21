354c42c76cc84ac97cee6318c2f7b8080c37f515
Evan Gibson <evangibson2794@gmail.com>
2020-05-21 00:18:22 -0400

    unstable png converter addition

--- fileprep_classification.py
+++ fileprep_classification.py
@@ -13,6 +13,7 @@ Description: This script is deisgned to move files from their original folders t
 # Useful for this type of file preparation
 import imagewiz.fw as fw
 
+import pydicom
 import pandas as pd
 import os
 
@@ -26,6 +27,25 @@ path_to_train_files = os.path.join(os.getcwd(),
                                    "projectfiles",
                                    "rsna-pneumonia-detection-challenge",
                                    "stage_2_train_images")
+
+def single_png_converter(path_to_png, destination_filepath):
+    ds = pydicom.dcmread(os.path.join(path + filename))
+    shape = ds.pixel_array.shape
+    # Convert to float to avoid overflow or underflow losses.
+    image_2d = ds.pixel_array.astype(float)
+
+    # Rescaling grey scale between 0-255
+    image_2d_scaled = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
+
+    # Convert to uint
+    image_2d_scaled = np.uint8(image_2d_scaled)
+
+    # defines the file type as png instead of dcm
+    p_filename = filename[:-3] + "png"
+
+    with open(os.path.join(dest_folder + p_filename), 'wb') as png_file:
+        w = png.Writer(shape[1], shape[0], greyscale=True)
+        w.write(png_file, image_2d_scaled)
 
 # Define main() function
 def main():
